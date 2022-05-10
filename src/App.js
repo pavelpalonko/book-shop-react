d8fcf58b93bed6feef57963444d31a70582329a6
pavel <pavelpalonko@gmail.com>
2022-05-10 22:31:11 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style/mainStyle.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
